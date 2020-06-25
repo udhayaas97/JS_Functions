9000c28f175e15c33549b57388412235c885fd29
Udhayakumar <udhayaas97@gmail.com>
2020-06-25 15:53:33 +0530

    Erorrs removed

--- JS/PatternStar.js
+++ JS/PatternStar.js
@@ -7,7 +7,6 @@
 //   * * * * *
 
 function star(n = 5) {
-    alert("Working");
     for (var i = 1; i <= n; i++) {
         for (var j = 1; j <= i; j++) {
             console.log("*");
--- JS/Temperature.js
+++ JS/Temperature.js
@@ -1,11 +1,14 @@
 // Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
 // [ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
 
-function Temperature(value, unit = "c") {
+function Temperature(value, unit = "C") {
+    unit = unit.toUpperCase();
     var result = 0;
-    if (unit == "f")
+    if (unit == "F") {
         result = (value - 32) * 5 / 9;
-    else if (unit == "c")
+        return value + "\u00B0" + unit + " is " + result + "\u00B0C"
+    } else if (unit == "C") {
         result = (value * 9 / 5) + 32;
-    return result;
+        return value + "\u00B0" + unit + " is " + result + "\u00B0F"
+    }
 }
